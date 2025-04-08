a4bf73991f2f43da27b1ba5e6d791476cf7c6627
Egho <egho.dpratama@gmail.com>
2025-04-08 12:47:53 +0700

    Add base config and deploy.yml for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/resume/',
+  base: '/egho-resume/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
